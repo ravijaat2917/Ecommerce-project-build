8e727b83278bf4917579a6203641739d4b14d20e
Ravi Lour <105196896+ravijaat2917@users.noreply.github.com>
2023-04-22 22:07:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename,"./client/build");
 // console.log(path.join(__dirname));
 app.use(express.static(path.join(__dirname)));
 
@@ -35,7 +35,6 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 //PORT
